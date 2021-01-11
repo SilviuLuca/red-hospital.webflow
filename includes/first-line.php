fd1b7ca206944e4d767f7e2d2a9d81f40c4258c4
SilviuLuca <49863501+SilviuLuca@users.noreply.github.com>
2021-01-11 18:32:32 +0200

    buton nou-rezultate analize

---
+++ includes/first-line.php
@@ -0,0 +1,28 @@
+<div class="first-line-top">
+  <div class="container-2 w-container">
+    <div class="phone-program-first-line">
+      <div class="text-block"></div><a href="tel:+40232230332" class="link">0232 230 332</a>
+      <div class="text-block-6"></div>
+      <div class="text-block-7">Lun-Vin 08:00 - 17:00</div>
+      <div class="text-block-8"></div>
+      <div class="text-block-9">office@sfsava.ro</div>
+    </div>
+    <div class="social-media-my-account">
+      <div class="rezultate">
+        <a href="https://rezultate.recumedis.ro" class="social-buttons" target="_blank">Rezultate Analize</a>
+        <a href="https://www.facebook.com/RedHospitalSpitalulSfSava/" class="social-buttons" target="_blank"></a>
+      </div>
+
+      <a href="spitale-formular-de-satistactie.php" target="_blank" class="link-block w-inline-block">
+        <div class="text-block-15 blanket formulare">Formulare</div>
+      </a>
+
+      <a href="http://clienti.recumedis.ro" target="_blank" class="link-block w-inline-block">
+        <div class="text-block-15 blanket">Acces Clienti</div>
+      </a>
+
+
+
+    </div>
+  </div>
+</div>
--- index.php
+++ index.php
@@ -45,25 +45,7 @@
 
 <body class="body">
 
-  <div class="first-line-top">
-    <div class="container-2 w-container">
-      <div class="phone-program-first-line">
-        <div class="text-block"></div><a href="tel:+40232230332" class="link">0232 230 332</a>
-        <div class="text-block-6"></div>
-        <div class="text-block-7">Lun-Vin 08:00 - 17:00</div>
-        <div class="text-block-8"></div>
-        <div class="text-block-9">office@sfsava.ro</div>
-      </div>
-      <div class="social-media-my-account"><a href="https://www.facebook.com/RedHospitalSpitalulSfSava/" class="social-buttons" target="_blank"></a>
-        <a href="spitale-formular-de-satistactie.php" target="_blank" class="link-block w-inline-block">
-          <div class="text-block-15 formulare">Formulare</div>
-        </a>
-        <a href="http://clienti.recumedis.ro" target="_blank" class="link-block w-inline-block">
-          <div class="text-block-15">Acces Clienti</div>
-        </a>
-      </div>
-    </div>
-  </div>
+  <?php include 'includes/first-line.php'?>
 
 
   <div class="logo-line">
